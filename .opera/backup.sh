343a61cccc8465442418e2a2f47df7f0885d0fd1
jpkotta <jpkotta@shannon>
2012-05-02 12:30:48 -0500

    remove icefiles and backup script for opera

--- .opera/backup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd $HOME
-rsync --archive --update --verbose \
-    --exclude "*cache*" --exclude "vps" --exclude temporary_downloads \
-    --exclude thumbnails --exclude images --exclude icons \
-    .opera/ opera-$(date +%F)
