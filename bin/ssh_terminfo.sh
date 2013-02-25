d5ba18bd51cf93ae9e9382d5b5151c67b46fd193
Jonathan Kotta <jpkotta@gmail.com>
2013-02-25 13:33:19 -0600

    add ssh_terminfo

---
+++ bin/ssh_terminfo.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ] ; then
+    echo "Usage: $0 [USER@]HOST"
+    echo "Copies terminfo for urxvt-unicode to a remote machine."
+    exit 1
+fi
+
+infocmp rxvt-unicode | ssh "$1" "mkdir -p .terminfo && cat > /tmp/ti && tic /tmp/ti"
